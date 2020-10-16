84e70b7ef5d46f604f95cef726ed04df9e98e990
Andrei Rezneac <andrei.rezneac@gmail.com>
2020-10-16 15:06:11 +0200

    Update readme, controller to always return json

--- README.md
+++ README.md
@@ -7,3 +7,20 @@ TerrestrialTimeUtcAdjustmentProvider implementation is not future-proof
 
 Very good brief explanation of the steps involved can be found here
 https://github.com/dkallen78/martian-clock
+
+to run the app (assuming you have PHP installed :D)
+      
+    from project root
+    cd ./public
+    php -S localhost:8000
+    
+    in your browser open: e.g.
+    http://localhost:8000/v1/earth-mars-times/2020-10-16
+
+to run the tests (from project root):
+    
+    bin/phpunit
+    
+useful commands  (from project root):
+    
+    bin/console
--- src/Controller/EarthMarsTimeController.php
+++ src/Controller/EarthMarsTimeController.php
@@ -4,13 +4,14 @@
 namespace App\Controller;
 
 use App\Logic\DateTimesCalculator;
-use \DateTime;
+use DateTime;
 use DateTimeInterface;
-use \DateTimeZone;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use \Throwable;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Throwable;
 
 class EarthMarsTimeController extends AbstractController
 {
@@ -30,6 +31,10 @@ class EarthMarsTimeController extends AbstractController
     function earthMarsTimesAction(string $gregorianUtcDateTime): JsonResponse
     {
         $gregorianUtc = $this->getGregorianUtc($gregorianUtcDateTime);
+        if ($gregorianUtc === null) {
+            return $this->getBadRequestResponse($gregorianUtcDateTime);
+        }
+
         $dateTimes = $this->earthMarsDateTimesCalculator->calculate($gregorianUtc);
 
         return JsonResponse::fromJsonString((string)$dateTimes);
@@ -38,17 +43,23 @@ class EarthMarsTimeController extends AbstractController
     /**
      * @throws BadRequestHttpException
      */
-    private function getGregorianUtc(string $input): DateTimeInterface
+    private function getGregorianUtc(string $input): ?DateTimeInterface
     {
         $utcTimeZone = new DateTimeZone('UTC');
         try {
             $gregorianUtcTime = new DateTime($input, $utcTimeZone);
         } catch (Throwable $exception) {
-            $message = "'$input' is invalid value for DateTime constructor.";
-            $hint = 'Check https://www.php.net/manual/en/datetime.construct.php';
-            throw new BadRequestHttpException($message . ' ' . $hint);
+            return null;
         }
 
         return $gregorianUtcTime;
+    }
+
+    private function getBadRequestResponse(string $gregorianUtcDateTime): JsonResponse
+    {
+        return new JsonResponse([
+            'message' => "'$gregorianUtcDateTime' is invalid value for DateTime constructor.",
+            'hint' => 'Check https://www.php.net/manual/en/datetime.construct.php'
+        ], Response::HTTP_BAD_REQUEST);
     }
 }
